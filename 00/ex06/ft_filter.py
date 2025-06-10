e26533208f50d603cf0ceaf3a70b2578b2990b8f
Benigne Demetz <benignedemetz@benigne.dev>
2025-06-10 17:59:11 +0200

    Ajout des fichiers filterstring.py et ft_filter.py avec la fonction ft_filter pour filtrer les éléments d'un itérable.

---
+++ 00/ex06/ft_filter.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+
+def ft_filter(function, iterable) -> list:
+    """filter(function or None, iterable) --> filter object
+
+Return an iterator yielding those items of iterable for which function(item)
+is true. If function is None, return the items that are true."""
+
+
+def main():
+    """Main function to execute the script."""
+    print(ft_filter.__doc__)
+    pass
+
+
+if __name__ == "__main__":
+    main()
