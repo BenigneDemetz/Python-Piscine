1cb72c2a5073f290491cad30455253a0375738a2
Benigne Demetz <benignedemetz@benigne.dev>
2025-06-11 16:36:22 +0200

    Ajout de la fonction principale dans filterstring.py et amélioration de la fonction ft_filter dans ft_filter.py pour un filtrage plus efficace des éléments d'un itérable.

--- 00/ex06/filterstring.py
+++ 00/ex06/filterstring.py
@@ -1,2 +1,22 @@
 #! /usr/bin/env python3
 
+import sys
+from benigne.get_args import get_args
+from benigne.ft_filter import ft_filter
+
+
+def main():
+    """Main function to execute the script."""
+    args: list[str] = get_args()
+    if len(args) != 2 or not args[1].isdigit():
+        print("AssertionError: the arguments are bad")
+        sys.exit(1)
+    words = args[0].split(' ')
+    value = lambda W: len(W) >= int(args[1])
+    print(list(ft_filter(value, words)))
+    
+
+if __name__ == "__main__":
+    main()
+
+    
--- 00/ex06/ft_filter.py
+++ 00/ex06/ft_filter.py
@@ -1,17 +1,31 @@
 #! /usr/bin/env python3
 
+# def ftprint(value):
+#     """Prints the value to the console."""
+#     if value < 4:
+#         return False
+#     return True
+
+
 def ft_filter(function, iterable) -> list:
     """filter(function or None, iterable) --> filter object
 
 Return an iterator yielding those items of iterable for which function(item)
 is true. If function is None, return the items that are true."""
 
+    if function is None:
+        return [item for item in iterable if item]
+    else:
+        return iter([item for item in iterable if function(item)])
 
-def main():
-    """Main function to execute the script."""
-    print(ft_filter.__doc__)
-    pass
+
+# def main():
+#     """Main function to execute the script."""
+#     print(ft_filter.__doc__)
+#     print(filter.__doc__)
+#     print(ft_filter(ftprint, [0, 1, 2, 3, 4, 5]))
+#     print(filter(ftprint, [0, 1, 2, 3, 4, 5]))
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
