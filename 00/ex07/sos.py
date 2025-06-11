29a70bf734eb602860ac65536e35bebfc54ae28e
benignedemetz <benignedemetz@benigne.dev>
2025-06-11 18:38:28 +0000

    Revert "ex07 done with argv"
    This reverts commit cffcaf340c7363489c61c24cf5a590ce0153b487.

--- 00/ex07/sos.py
+++ 00/ex07/sos.py
@@ -1,26 +1,27 @@
 #!/usr/bin/env python3
 
-import sys
-import getopt
+from benigne.get_args import get_args
+from benigne.ft_filter import ft_filter
+
+NESTED_MORSE = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.',
+    'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..',
+    'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.',
+    'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+    'Y': '-.--', 'Z': '--..', ' ': '/', '0': '-----', '1': '.----', '2': '..---',
+    '3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...',
+    '8': '---..', '9': '----.'
+}
+
 
 def main():
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "ha:v", ["help", "age=", "verbose"])
-    except getopt.GetoptError as err:
-        print(err)
-        sys.exit(2)
-    
-    verbose = False
-    age = None
-    print(opts, args)
-    for opt, arg in opts:
-        if opt in ("-h", "--help"):
-            print("Usage: script.py -a <age> -v")
-            sys.exit()
-        elif opt in ("-a", "--age"):
-            age = int(arg)
-        elif opt in ("-v", "--verbose"):
-            verbose = True
+    args: list[str] = get_args()
+    if len(args) != 1 or not args[0].replace(" ", "").isalnum():
+        print("AssertionError: the arguments are bad")
+        return
+    string = args[0].upper()
+    ft_filter(lambda c: print(NESTED_MORSE[c], end=' '), string)
+    print()
 
 if __name__ == "__main__":
     main()
