ae2e5095963994d642eb0d15a3d4a10c9cff727e
Benigne Demetz <benignedemetz@benigne.dev>
2025-06-13 11:41:40 +0200

    done

--- 00/ex08/Loading.py
+++ 00/ex08/Loading.py
@@ -1,42 +1,20 @@
 #!/usr/bin/env python3
 
-from time import time
-import os
 
-def set_string(i, speed, start_time, lst) -> str:
+def set_string(i, lst) -> str:
     """
     Set the string to be printed in the progress bar.
     """
-    remaining_time = "?"
     size = len(lst)-1
     try:
-        termsize = os.get_terminal_size().columns
         char = '█'
     except OSError:
         char = '='
-        termsize = 80
-    if i > 1:
-        remaining_time = (time() - start_time) * (size - i) / i
-        if remaining_time < 0:
-            remaining_time = 0
-        min = int(remaining_time // 60)
-        snd = int(remaining_time % 60)
-        remaining_time = str(f"{min:02}:{snd:02}")
+    termsize = 80
     data = ""
-    elapsed_time = time() - start_time
-    min = int(elapsed_time // 60)
-    snd = int(elapsed_time % 60)
-    elapsed_time = str(f"{min:02}:{snd:02}")
-    if speed is not None:
-        speed = (time() - speed)
-        speed = f"{1 / speed:.2f}"
-    else:
-        speed = '?'
-
-    speedpad = ' ' if len(speed) < 5 else ''
     
     begin = f"{int(i/size*100)}%|"
-    end = f"| {i}/{size+1} [{elapsed_time}<{remaining_time}, {speedpad}{speed}it/s]"
+    end = f"| {i}/{size+1}"
     total = termsize - len(begin + end)
     for j in range(int(i * (total) / size)):
         data += char
@@ -48,14 +26,12 @@ def set_string(i, speed, start_time, lst) -> str:
 
 
 def ft_tqdm(lst: range) -> None:
-    start_time = time()
     speed = None
     for i in lst:
-        string = set_string(i, speed, start_time, lst)
+        string = set_string(i, lst)
         print('\r' + string, end="", flush=True)
-        speed = time()
         yield i
-    string = set_string(i+1, speed, start_time, lst)
+    string = set_string(i+1, lst)
     print('\r' + string, end="", flush=True)
     # print()
     return None
